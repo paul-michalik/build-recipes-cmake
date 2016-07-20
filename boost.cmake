8938fa16e960434ea8d5cc9dfc913f66118b031c
Paul Michalik <paul.michalik@outlook.com>
2016-07-20 17:12:52 +0200

    Update boost.cmake

--- boost.cmake
+++ boost.cmake
@@ -1,14 +1,11 @@
 cmake_minimum_required(VERSION 3.0 FATAL_ERROR)
 
 # Boost configuration begin
-# We switch off autolinking support in order to be compatible on all platforms:
+# We switch off autolinking support in order to have compatible behaviour on all platforms:
 add_definitions(-DBOOST_ALL_NO_LIB)
 
 # We require usage of static libraries on all platforms:
 set(Boost_USE_STATIC_LIBS ON)
-
-# We require usage of dynamic libraries on all platforms:
-# add_definitions(-DBOOST_ALL_NODYN_LINK)
 
 # We explicitely list all required libraries. This has the advantage of having a single point of failure if one of the libraries is missing:
 find_package(Boost 1.61 EXACT REQUIRED
