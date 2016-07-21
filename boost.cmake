e1c7689cf2d1e31a1bb969f6b2a2b07ae4dac387
PMichalik <paul.michalik@harman.com>
2016-07-21 12:21:56 +0200

    Prepared boost.cmake

--- boost.cmake
+++ boost.cmake
@@ -1,17 +1,21 @@
 cmake_minimum_required(VERSION 3.0 FATAL_ERROR)
 
+#include include the devkit system file
+    
+# prepare the boost variables
+    
+# We explicitely list all required libraries. This has the advantage of having a single point of failure if one of the libraries is missing:
+find_package(Boost 1.61 EXACT REQUIRED
+    system 
+    filesystem 
+    unit_test_framework) # add other libraries as required!
+
 # Boost configuration begin
 # We switch off autolinking support in order to have compatible behaviour on all platforms:
 add_definitions(-DBOOST_ALL_NO_LIB)
 
 # We require usage of static libraries on all platforms:
 set(Boost_USE_STATIC_LIBS ON)
-
-# We explicitely list all required libraries. This has the advantage of having a single point of failure if one of the libraries is missing:
-find_package(Boost 1.61 EXACT REQUIRED
-    system 
-    filesystem 
-    unit_test_framework) # add other libraries as required!
 
 # Compiler will find boost includes:
 include_directories(${Boost_INCLUDE_DIRS}) 
