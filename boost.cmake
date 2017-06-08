345599f72fa827d23d331e8cc1349ba8479917ba
PMichalik <paul.michalik@harman.com>
2017-06-08 16:23:58 +0200

    Removed pollution from boost.cmake. Needs modern CMake >= 3.0

--- boost.cmake
+++ boost.cmake
@@ -1,11 +1,15 @@
-#include include the devkit system file    
+#include the devkit system file
     
 # Boost configuration begin
 # We switch off autolinking support in order to have compatible behaviour on all platforms:
+# Alternatively we could be using "Boost::disable_autolinking" per target but this guarantees
+# a consistent setting for all targets 
 add_definitions(-DBOOST_ALL_NO_LIB)
 
-# We require usage of static libraries on all platforms:
-set(Boost_USE_STATIC_LIBS ON)
+# We require this configuration on all platforms:
+set(Boost_USE_STATIC_LIBS        ON)
+set(Boost_USE_MULTITHREADED      ON)
+set(Boost_USE_STATIC_RUNTIME    OFF)
 
 # We explicitely list all required libraries. This has the advantage of 
 # having a single point of failure if one of the libraries is missing.
@@ -14,12 +18,6 @@ set(Boost_USE_STATIC_LIBS ON)
 # exact specifications we need to become exact again.
 find_package(Boost REQUIRED
     unit_test_framework
+    filesystem
     # add or remove libraries as required!
-    ) 
-
-# Compiler will find boost includes:
-include_directories(${Boost_INCLUDE_DIRS}) 
-
-# Linker will find boost libraries:
-link_directories(${Boost_LIBRARY_DIRS}) 
-# Boost configuration end file contents here
+)
